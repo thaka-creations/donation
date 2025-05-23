9f03ecd5994fde431189c1dc566633ae36f04092
Abdul Nelfrank <nelfrankjunior@gmail.com>
2025-05-23 18:36:26 +0300

    Update auth.ts to use a hardcoded API base URL for immediate access, replacing the previous environment variable configuration. This change simplifies the setup but may limit flexibility for different environments.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE_URL = "http://45.79.97.25:8013/api/v1";
 
 if (!API_BASE_URL) {
   throw new Error('API URL is not defined in environment variables');
